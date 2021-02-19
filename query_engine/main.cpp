ba44e4cdc856075fecf110a61f8ea1f1c5a9f831
Ankit Srivastava <ank7845@gmail.com>
2021-02-18 18:40:32 -0800

    Initial Commit

---
+++ query_engine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  query_engine
+//
+//  Created by Ankit Srivastava on 2/18/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
